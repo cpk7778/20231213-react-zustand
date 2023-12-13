e9de2133741aaeb64936e0c8458ae89f0d3edc26
cpk7778 <cpk7778@gmail.com>
2023-12-13 22:08:08 +0900

    20231213-home

---
+++ memo.md
@@ -0,0 +1,13 @@
+# 설치
+
+npx create-react-app my-app
+
+npm install zustand
+
+npm start
+
+# tailwind 설치
+
+npm install tailwindcss postcss-cli autoprefixer
+
+npx tailwindcss init -p
--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import create from 'zustand';
+
+const useStore = create((set) => ({
+  count: 0,
+  increaseCount: () => set((state) => ({ count: state.count + 1 })),
+}));
 
 function App() {
+  const { count, increaseCount } = useStore();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="text-2xl font-semibold">구독자 {count}</p>
+      <button
+        onClick={increaseCount}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Increase Count
+      </button>
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+  const { count } = useStore();
+
+  return (
+    <div className>
+      <p>카드 {count} </p>
     </div>
   );
 }
